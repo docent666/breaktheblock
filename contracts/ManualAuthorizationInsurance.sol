2375efc8015f4c26532051f4b26c9e995491dc3f
Docent <lukasz.musial@gmail.com>
2017-07-23 09:57:48 +0100

    manually authorized flavour of the insurance contract

--- contracts/ManualAuthorizationInsurance.sol
+++ contracts/ManualAuthorizationInsurance.sol
@@ -3,4 +3,34 @@ pragma solidity ^0.4.2;
 import './Insurance.sol';
 
 contract ManualAuthorizationInsurance is Insurance {
+
+    address owner;
+    address authoriser;
+
+    event RequestAuthorisation(address claimant);
+
+    function ManualAuthorizationInsurance() {
+        owner = msg.sender;
+    }
+
+    modifier owner_only() {
+        require(msg.sender == owner);
+        _;
+    }
+
+    function assignAuthoriser(address auth) owner_only {
+        authoriser = auth;
+    }
+
+    function claim() {
+        RequestAuthorisation(msg.sender);
+    }
+
+    function authoriseClaim(address claimant) {
+        if (msg.sender == authoriser && insurances[claimant].exists) {
+            insurances[claimant].withdrawable = true;
+        }
+    }
+
+
 }
---
+++ test/manualauthorizationinsurance.js
@@ -0,0 +1,50 @@
+var Insurance = artifacts.require("./ManualAuthorizationInsurance.sol");
+
+
+var multiplier = Math.pow(10,16)
+
+contract('ManualAuthorizationInsurance', function(accounts) {
+
+   //truffle will by default assign accounts[0] as na owner of the deployed contract
+  const account_sponsor = accounts[0];
+  const account_one = accounts[1];
+  const account_authorizer = accounts[2];
+
+  const amount = multiplier*88;
+
+  function humanReadableBalance(account) {
+     return Math.floor(web3.eth.getBalance(account).toNumber()/multiplier);
+  }
+
+  it("when an insurance claim is made on a seeded pool then insured amount can be withdrawn only when authorised", function () {
+    var insurance;
+
+    var account_one_starting_balance = humanReadableBalance(account_one);
+    var insureAmount = multiplier*23;
+    var poolSize = multiplier*100;
+    var premium = multiplier*15;
+    var amountToVerify = (insureAmount - premium)/ multiplier;
+    var timestamp = web3.eth.getBlock(web3.eth.blockNumber).timestamp
+
+    return Insurance.deployed().then(function(instance) {
+        insurance = instance;
+        return insurance.init.sendTransaction(poolSize, poolSize/2, timestamp, {from: account_sponsor})
+    }).then(function() {
+        return insurance.contribute.sendTransaction({from: account_sponsor, value: insureAmount + 10})
+    }).then(function() {
+        return insurance.insure.sendTransaction(insureAmount, {from:account_one, value: premium});
+    }).then(function() {
+        return insurance.claim.sendTransaction({from:account_one});
+    }).then(function() {
+        return insurance.assignAuthoriser.sendTransaction(account_authorizer, {from:account_sponsor});
+    }).then(function() {
+        return insurance.authoriseClaim.sendTransaction(account_one, {from:account_authorizer});
+    }).then(function() {
+        return insurance.withdraw.sendTransaction({from:account_one});
+    }).then(function() {
+        var account_one_ending_balance = humanReadableBalance(account_one);
+        assert.approximately(account_one_ending_balance, account_one_starting_balance + amountToVerify, 3, "claim not recorded properly");
+    }).catch((err) => { throw new Error(err) })
+  })
+
+});
